7ee56de2c67343f52eeabc1ca53b6ec6e1d917b4
cjvrd <cjvirdo@gmail.com>
2024-04-29 20:55:42 +1000

    changes for 6.2c

--- Week_5/server.js
+++ Week_5/server.js
@@ -10,3 +10,11 @@ const port = process.env.port || 3000; //defines port to run server on
 app.listen(port, () => { //tells express which port to listen on and logs when started
     console.log("App listening on: "+ port) 
 });
+
+
+//change for task 6.2C
+app.get("/", (req, res) => {
+    const n1 = "<html><body><H1>HELLO THERE THIS IS MY CHANGE FOR TASK 6.2C </H1></body></html>";
+    res.set('Content-Type', 'text/html');
+    res.send(Buffer.from(n1));     
+})
